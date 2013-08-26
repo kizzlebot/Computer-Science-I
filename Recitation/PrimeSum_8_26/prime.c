84bfbc4a54fc1018a011a11e4908965fa25f0ec1
James Choi <jchoi2012@knights.ucf.edu>
2013-08-26 16:02:49 -0400

    primes.c

--- Recitation/PrimeSum_8_26/prime.c
+++ Recitation/PrimeSum_8_26/prime.c
@@ -11,11 +11,11 @@ int main(){
 int * sieveOfEratosthenes(int numPrimesToFind){
 	int i = 0 ;
 	int j = 0 ;
-	int * consecutive = malloc(sizeof(int)*numPrimesToFind+1);
+	int * consecutive = malloc(sizeof(int)*(numPrimesToFind+5));
 	
 	// Fill up the first two indexes with whatevers bc if its zero, it doesn't matter
-	consecutive[0] = 0 ;
-	consecutive[1] = 0 ; 
+	consecutive[0] = 2 ;
+	consecutive[1] = 3 ; 
 	// start getting rid of all multiples 
 	for ( i = 2 ; i < numPrimesToFind;  ){
 		// Only fill up to numPrimesToFind if first iteration
@@ -41,13 +41,13 @@ int * sieveOfEratosthenes(int numPrimesToFind){
 
 	// Reset J and use it to count the number of primes
 	j = 0 ; 
-	for ( i = 0  ; i< numPrimesToFind; i++){
+	for ( i = 2  ; i< numPrimesToFind+5; i++){
 		if ( consecutive[i] != 0 ){
 			j++ ; 
 		}
 	}
 	// Use the count to malloc that exact size
-	int * primesOnly = malloc(sizeof(int)*j+1);
+	int * primesOnly = malloc(sizeof(int)*(j+1));
 	j = 0 ; 
 	for ( i = 0 ; i < numPrimesToFind; i++){
 		if (consecutive[i] != 0 ){
@@ -56,62 +56,35 @@ int * sieveOfEratosthenes(int numPrimesToFind){
 		}
 	}
 	free(consecutive);
-	//for ( i = 0 ; i < j; i++ ){
-	//	printf("%d\n",primesOnly[i]);
-	//}
+	for ( i = 0 ; i < j; i++ ){
+		//printf("%d\n",primesOnly[i]);
+	}
 	return primesOnly ; 
 }
 void readFile(){
 	FILE * infile;
-	FILE * outfile;
+	FILE * outfile = fopen( "primesum1.out" , "w");
 	// Open file for reading
-	infile  = fopen(  "primesum.in",   "r" );
+	infile = fopen("primesum.in","r");
 	int temp ; 
 	int readLines= 0 ;  
 	int * primeList ; 
 	while (fscanf(infile,"%d",&temp)!= EOF){
 		if ( readLines == 0 ){
 			primeList = sieveOfEratosthenes(temp);	
-		}
-		else{
+			readLines++;	
+			continue ; 
+		}else{
 			printSum(primeList,temp);
 		}
-		readLines++;	
 	}			
 }
-
-
-void printSum(int* primeList ,int n ){
-	int i = 0 ;
+void printSum(int* primeList ,int n){
+	int i  ;
 	int sum = 0 ;  
 	for ( i = 0 ; i < n ; i++){
 		sum+=primeList[i] ; 	
 	}
 	printf("%d\n",sum);
-}
-int* generatePrime(int n ){
-	int i ;
-	int j ;
-	int nthprime = n; 
-	int currentprime = 8	 ; 
-	int* primelist = malloc(sizeof(int)*nthprime) ; 
-	// put a couple of primes in 
-	primelist[0] = 2;
-	primelist[1] = 3;
-	primelist[2] = 5;
-	primelist[3] = 7;
-	// 
-	for ( i = 3 ; i < nthprime ;){
-		for ( j = 2 ; j < currentprime ; j++){
-			if ( currentprime%j == 0 ){
-				break ; 
-			}
-		}
-		if ( j == currentprime ){
-			i++;
-			primelist[i] = currentprime ; 
-		}
-		currentprime++ ; 
-	}
-	return primelist;
+	//fprintf(outfile,"%d\n",sum);
 }
