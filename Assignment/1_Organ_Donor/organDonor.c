5564f4ca478209bc05fad2273d5074bf2c6cbc60
James Choi <jchoi2012@knights.ucf.edu>
2013-08-28 14:04:24 -0400

    I guessed the inputfile for assignment 1 because Arup didnt post that shit

--- Assignment/1_Organ_Donor/organDonor.c
+++ Assignment/1_Organ_Donor/organDonor.c
@@ -1,32 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define SIZE 20
 #define BLOODTYPESIZE 4
-
-
-
-int main(){
-	// Each line will contain the 
-	// person's name 
-	// the organ they need replaced
-	// their blood type 
-	// the date they were added to the organ database 
-	// and the time they were added to the organ database	
-	FILE * infile = fopen("organ.in","r");
-	int numLines ;
-	fscanf(infile,"%d",&numLines);
-	organT * patient = (organT * )malloc(sizeof(organT)*numLines);
-	
-	int i = 0 ; 
-	patient[i]->dateAdded = (dateT *)malloc(sizeof(dateT));
-	patient[i]->timeAdded = (timeT *)malloc(size(timeT));
-	while ( fscanf(infile,"%d %d %d %d %d %d",patient[i].name,patient[i].organname,patient[i].bloodtype,patient.dateAdded,patient.timeAdded,patient.received)!=EOF){
-		i++ ; 
-		patient[i]->dateAdded = (dateT *)malloc(sizeof(dateT));
-		patient[i]->timeAdded = (timeT *)malloc(size(timeT));
-	}
-}
 
 
 typedef struct {
@@ -41,11 +16,57 @@ typedef struct {
 } timeT;
 
 typedef struct {
-	char name[SIZE];
-	char organname[SIZE];
+	char * name;
+	char * organname;
 	char bloodtype[BLOODTYPESIZE];
-	dateT dateAdded;
-	timeT timeAdded;
+	char * dateAdded;
+	char * timeAdded;
 	int received;
 } organT;
+
+organT ** allocate(int numLines){
+	organT ** patient = (organT ** )malloc(sizeof(organT *)*(numLines+1));
+	int i = 0 ; 
+	for ( i = 0 ; i < numLines ; i++ ){
+		patient[i]  = (organT * )malloc(sizeof(organT ));
+		patient[i]->name= (char *)malloc(sizeof(char)*SIZE);
+		patient[i]->organname = (char *)malloc(sizeof(char)*SIZE);
+		patient[i]->dateAdded = (char*)malloc(sizeof(char )*SIZE);
+		patient[i]->timeAdded = (char *)malloc(sizeof(char )*SIZE);
+		patient[i]->received = 0 ;
+	}
+	patient[i] = NULL ; 
+	return patient ; 
+}
+void printPatient(organT * patient){
+	if ( patient != NULL ){
+		printf("     Name: %s\n",patient->name);
+		printf("    Organ: %s\n",patient->organname);
+		printf("BloodType: %s\n",patient->bloodtype);
+		printf("    Added: %s\n",patient->dateAdded);
+		printf("    tAdded: %s\n\n",patient->timeAdded);
+
+	}
+}
+int main(){
+	// Each line will contain the 
+	// person's name 
+	// the organ they need replaced
+	// their blood type 
+	// the date they were added to the organ database 
+	// and the time they were added to the organ database	
+	FILE * infile = fopen("organ.in","r");
+	int numLines ;
+	fscanf(infile,"%d",&numLines);
+
+	organT ** patient = allocate(numLines);
+	
+	int i = 0 ; 
+	while ( i < numLines ){
+		fscanf(infile,"%s %s %s %s %s",(patient[i]->name),(patient[i]->organname),(patient[i]->bloodtype),(patient[i]->dateAdded),(patient[i]->timeAdded));
+		printPatient(patient[i]);
+		i++ ; 
+	}
+}
+
 
