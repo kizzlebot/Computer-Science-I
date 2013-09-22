ee1226a02f044abee5e982458f0a6eaaf26be024
James Choi <jchoi2012@knights.ucf.edu>
2013-09-22 02:29:58 -0400

    figured out that algorithm

--- Assignment/2_Passwords/passwords.c
+++ Assignment/2_Passwords/passwords.c
@@ -1,76 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *  {b,c,d,a}
- *  {a,b,c}
- *  {t,e,r,k}
+#include <string.h>
+#define GUESS 10000
+
+/*
+ * Takes an array of words, and returns the value of
+ * strlen(words[i-1])*strlen(words[i-2])*strlen....
  */
-char ** alloc( char * ch, int * setLengths ){
-    int max_size = 10 ;
-    char ** dblArray = (char ** )malloc(sizeof(char *)*max_size);
-    int i = 0 ;
-    while ( setLengths[i] != '\0'){
-        int j = 0 ;
+int getCombosPerMove(char ** words  ){
+    if ( *words == NULL ) return 1 ;
+    return strlen(*(words)) * getCombosPerMove(words+1);
+}
+void findCombo(char ** words , int magic  ){
+    if ( *words == NULL ) return ;
+    int combosPerMove = getCombosPerMove(words+1);
+    printf("%c",*(*(words)+magic/combosPerMove));
+    findCombo(words+1,magic%combosPerMove);
+}
+char ** read(char ** sets , int numSets){
+    if (numSets == 0 ) return sets ;
 
-        dblArray[i] = (char *)malloc(sizeof(char)*(setLengths[i]+1));
-        for ( j =0 ; j < setLengths[i] ; j++){
-            if ( ch[j] != '\0'){
-                dblArray[i][j] = ch[j] ;
-            }
-            else{
-                dblArray[i] = (char *)realloc(dblArray[i],(j+1));
-                dblArray[i][j] = '\0';
-                break;
-            }
-        }
-        if ( j < setLengths[i] ){
-            continue ;
-        }
-        else{
-            dblArray[i][setLengths[i]] = '\0' ;
-        }
-        i++ ;
-        if ( i >= max_size ){
-            max_size*=2 ;
-            char ** dblArray = (char ** )malloc(sizeof(char *)*max_size);
-        }
-    }
-    return dblArray ;
+    char * aLine ;
+    aLine = (char *) calloc(GUESS,sizeof(char));
+    scanf("%s",aLine );
 
+    *sets = (char *)calloc(strlen(aLine)+1,sizeof(char )) ;
+    strcpy(*sets,aLine);
+    free(aLine);
+
+    read(sets+1,numSets-1);
+    return sets ;
+}
+void recursiveFree(char ** sets  ){
+    if (*sets == NULL ) return ;
+
+    recursiveFree(sets+1);
+    free(*sets);
+}
+void recursiveInit(char ** input , int numPassesToGuess){
+    if ( numPassesToGuess == 0 ) return ;
+
+    int numSets ;
+    int magicNumber ;
+    scanf("%d",&numSets);
+    input = read( (char **) calloc(numSets+1,sizeof(char *)),numSets);
+    scanf("%d",&magicNumber);
+
+    findCombo(input,magicNumber-1);
+
+    printf("\n");
+    recursiveFree(input);
+    recursiveInit(input,numPassesToGuess-1);
 }
 
-void printList(char ** list){
-    int i = 0 ;
-    while ( list[i] != '\0'){
-        int j =0 ;
-        printf("Set #%d: ",i);
-        while (list[i][j] != '\0'){
-            printf("%c ",list[i][j]);
-            j++ ;
-        }
-        printf("\n");
-        i++ ;
-    }
-}
-void extend_sets(char * base , char ** set,int n,int k){
-    if ( set[n][k] == '\0'){
-        printf("%s\n",set[n]);
-    }
-    else{
-        char c = set[n][k] ;
-        int i = 0  ;
-        for ( i = 0 ; i <= k ; i++){
-        }
-    }
-}
 
 int main(){
-    int * in = (int *) malloc(sizeof(int)*20);
-    in[0] = 4;
-    in[1] = 3;
-    in[2] = 4;
-    in[3] = 3;
-    char ** ch = alloc( "BLAHBLAHBLAHBLAH", in);
-    printList(ch);
-
+    int numPassesToGuess;
+    // Get the first line containg number of passwords to guess
+    scanf("%d",&numPassesToGuess);
+    char ** input ;
+    recursiveInit(input,numPassesToGuess);
 }
