ec26b0d01d7b5d010eb893f3eeb78cff3e5bef9a
James Choi <jchoi2012@knights.ucf.edu>
2013-09-29 22:54:04 -0400

    still wroking on it

--- Assignment/3_Organ_bin-tree/organ.c
+++ Assignment/3_Organ_bin-tree/organ.c
@@ -12,7 +12,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define SIZE 20
 #define BLOODTYPESIZE 4
@@ -49,40 +48,128 @@ typedef struct {
     char * organ ;
     char * bloodType ;
 }Donor ;
+struct Node {
+    organT patient;
+    struct Node * left ;
+    struct Node * right ;
+} ;
 
-void printPatient(organT ** patientList){
-	if ( *patientList == NULL ){
-		return ; 
+void printPatient(organT * patientList){
+	if ( patientList == NULL ){
+		return ;
 	}
-	printf("name %s\n",(*patientList)->name);
-	printf("organ:%s\n",(*patientList)->organname);
-	printf("bloodtype:%s\n",(*patientList)->bloodtype);
-	printf("DateAdded:%d/%d/%d\n",(*patientList)->dateAdded.month,(*patientList)->dateAdded.day,(*patientList)->dateAdded.year);
-	printf("TimeAdded:%d:%d\n\n",(*patientList)->timeAdded.hour,(*patientList)->timeAdded.minute);
+	printf("name %s\n",(patientList)->name);
+	printf("organ:%s\n",(patientList)->organname);
+	printf("bloodtype:%s\n",(patientList)->bloodtype);
+	printf("DateAdded:%d/%d/%d\n",(patientList)->dateAdded.month,(patientList)->dateAdded.day,(patientList)->dateAdded.year);
+	printf("TimeAdded:%d:%d\n\n",(patientList)->timeAdded.hour,(patientList)->timeAdded.minute);
 	printPatient(patientList+1);
 }
 
-void read(int count ,organT ** patientList ){
+void read(int count ,organT * patientList ){
 	if (count == 0 ){
-		return ; 
+		return ;
 	}
-	char ch ; 
-	*patientList = (organT *)malloc(sizeof(organT));
-	(*patientList)->name = (char *) malloc(sizeof(char)*26);
-	(*patientList)->organname = (char *) malloc(sizeof(char)*26);
-	scanf("%s %s %s %d%c%d%c%d %d%c%d",((*patientList)->name),((*patientList)->organname),((*patientList)->bloodtype),
+	char ch ;
+	(patientList)->name = (char *) calloc(50,sizeof(char));
+	(patientList)->organname = (char *) calloc(50,sizeof(char));
+	scanf("%s %s %s %d%c%d%c%d %d%c%d",((patientList)->name),((patientList)->organname),((patientList)->bloodtype),
 								   //  Month            /        Day            /      year
-								   &((*patientList)->dateAdded.month),(&ch),&((*patientList)->dateAdded.day),(&ch),&((*patientList)->dateAdded.year),
+								   &((patientList)->dateAdded.month),(&ch),&((patientList)->dateAdded.day),(&ch),&((patientList)->dateAdded.year),
 								   //    Hour           :      Minute
-								   &((*patientList)->timeAdded.hour),(&ch),&((*patientList)->timeAdded.minute));
+								   &((patientList)->timeAdded.hour),(&ch),&((patientList)->timeAdded.minute));
 	read(count-1,patientList+1);
 
 }
 
-int main(){
-	int numPatients ; 
-	scanf("%d",&numPatients);
-	organT ** patientList = (organT **)malloc(sizeof(organT*)*(numPatients+1));
-	read(numPatients,patientList);
-	printPatient(patientList);
+struct Node * createNode(char * name , char * organname , char * bloodType ,  int month, int day, int year ){
+    struct Node * aNode = (struct Node *) calloc(1,sizeof(struct Node));
+    aNode->patient.name = (char *)calloc(40,sizeof(char));
+    aNode->patient.organname= (char *)calloc(40,sizeof(char));
+    strcpy(aNode->patient.name, name) ;
+    strcpy(aNode->patient.organname,organname);
+    strcpy(aNode->patient.bloodtype,bloodType);
+    aNode->patient.dateAdded.month = month ;
+    aNode->patient.dateAdded.day= day;
+    aNode->patient.dateAdded.year = year;
+    aNode->left = NULL ;
+    aNode->right = NULL ;
+    return aNode ;
 }
+struct Node * insertPatient(struct Node * root, struct Node * element ){
+    if ( root == NULL ){
+        return element;
+    }
+    if ( element->patient.dateAdded.year > root->patient.dateAdded.year ){
+        if (root->right != NULL ){
+            root->right = insertPatient(root->right,element);
+        }
+        else {
+            root->right = element ;
+        }
+    }
+    else if ( element->patient.dateAdded.year  == root->patient.dateAdded.year ){
+        printf("year same than \n");
+        if(element->patient.dateAdded.month > root->patient.dateAdded.month) {
+            printf("month differ than \n");
+            if (root->right != NULL ){
+                root->right = insertPatient(root->right,element);
+            }
+            else {
+                root->right = element ;
+            }
+        }
+        else if(element->patient.dateAdded.month == root->patient.dateAdded.month) {
+            printf("month same than \n");
+            if (element->patient.dateAdded.day >= root->patient.dateAdded.day){
+                printf("day differ than \n");
+                if (root->right != NULL ){
+                    root->right = insertPatient(root->right,element);
+                }
+                else {
+                    root->right = element ;
+                }
+            }
+            else{
+                printf("day equal or less than \n");
+                if (root->left != NULL ){
+                    root->left = insertPatient(root->left,element);
+                }
+                else{
+                    root->left = element ;
+                }
+            }
+        }
+        else{
+            if (root->left != NULL ){
+                root->left = insertPatient(root->left,element);
+            }
+            else{
+                root->left = element ;
+            }
+        }
+    }
+    else{
+    printf("year less than ");
+        if (root->left != NULL ){
+            root->left = insertPatient(root->left,element);
+        }
+        else{
+            root->left = element ;
+        }
+    }
+    return root;
+
+}
+int main(){
+	int numPatients ;
+	//scanf("%d",&numPatients);
+    struct Node * aNode = NULL;
+    aNode = createNode("James","Choi","A",2,24,1990);
+    aNode = insertPatient(aNode,createNode("JOhnny","Kidney","A",2,25,1990));
+    printf("%s ",aNode->right->patient.name);
+    //printf("%s ",aNode->left->patient.organname);
+    //printf("%s ",aNode->left->patient.bloodtype);
+    return 0 ;
+}
+
