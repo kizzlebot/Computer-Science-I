46d34f351ec8a57b1f6de35a2375e273173f5498
James Choi <jchoi2012@knights.ucf.edu>
2013-10-09 21:16:10 -0400

    okay

---
+++ Assignment/3_maze/maze.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * Queue is part of Breadth First Search
+ *
+ * Algorithm ;
+ *	. Given a starting point,
+ *	a.) Travel to all adjacent places, mark these locations as being 1 step away
+ *	from starting point
+ *	b.) For each location that is 1 step away, visit all adjacent points that are not
+ *	visited, mark these locations as being two steps away.
+ *	c.) Repeat until one of the spots you mark is the exit
+ */
+
+int main(){
+}
