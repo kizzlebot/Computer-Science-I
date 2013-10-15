59bdbc3919fba1a0e766ae930d7f93f896d775ea
kizzlebot <jchoi2012@knights.ucf.edu>
2013-10-15 03:51:40 -0400

    Cleaned it up mane

--- Assignment/3_maze/maze.c
+++ Assignment/3_maze/maze.c
@@ -1,16 +1,15 @@
+/*
+ * Name:          James Choi
+ * Course:        COP 3502 Computer-Science I (Mon,Wed,Fri)
+ * Assignment #3: Maze.c
+ */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 /*
- * Queue is part of Breadth First Search
- *
- * Algorithm ;
- *	. Given a starting point,
- *	a.) Travel to all adjacent places, mark these locations as being 1 step away
- *	from starting point
- *	b.) For each location that is 1 step away, visit all adjacent points that are not
- *	visited, mark these locations as being two steps away.
- *	c.) Repeat until one of the spots you mark is the exit
+ * I got stacks..
+ * Linked list implementation of stacks used as queues containing coords as int[]
  */
 struct stack{
     int coord[2];
@@ -18,70 +17,86 @@ struct stack{
 };
 
 
-
+/////////////////////////////////// Stack Functions //////////////////////////////////////////////
 /*    Function: alloc
- *    Arguments: (int x, int y ) X and Y Coords
- *    Return: (struct stack * ) Newly allocated node, that has a nextVal of NULL and X,Y coords as given
- *    Description: Allocates and returns a node that points to null with coords.  */
+ *   Arguments: (int x, int y ) X and Y Coords
+ *      Return: (struct stack * ) Newly allocated node, that has a nextVal of NULL and X,Y coords as given
+ * Description: Allocates and returns a node that points to null with coords.  */
 struct stack * alloc(int x, int y );
 
 /*    Function: push
- *    Arguments: (struct stack ** root) Pointer to the root node, which is top of stack
- *    (struct stack * newTop) node to add as the new top
- *    Return: Returns pointer to the top of the stack ;
- *    Description: Push newTop to the top of the stack  */
+ *   Arguments: (struct stack ** root) Pointer to the root node, which is top of stack
+ *              (struct stack * newTop) node to add as the new top
+ *      Return: Returns pointer to the top of the stack ;
+ * Description: Push newTop to the top of the stack  */
 struct stack * push(struct stack * root, struct stack * newTop);
 
 /*    Function: pop
- *    Arguments: (struct stack ** root) Pointer to the root node, which is top of stack
- *    Return: (struct stack * ) Element that was on top of stack
- *    Description: Given the root node, modify the stack by removing the topmost element and returning it*/
+ *   Arguments: (struct stack ** root) Pointer to the root node, which is top of stack
+ *      Return: (struct stack * ) Element that was on top of stack
+ * Description: Given the root node, modify the stack by removing the topmost element and returning it*/
 struct stack * pop(struct stack ** root);
 
-/*    Function: popBottom
- *    Arguments: (struct stack ** root) Pointer to the root node, which is top of stack
- *    Return: (struct stack * ) Element that was on bottom of stack
- *    Description: Given the root node, modify the stack by removing the bottommost element and returning it*/
-struct stack * popBottom(struct stack ** root);
+/////////////////////////////////// Stack Functions END //////////////////////////////////////////
 
-/*    Function: getBottomPtr
- *    Arguments: (struct stack ** root) Pointer to the root node, which is top of stack
- *    Return: (struct stack *) The pointer to the bottom of stack w/o modifying the stack ;
- *    Description: Returns the pointer to the bottom most element w/o modifying the stack  */
-struct stack * getBottomPtr(struct stack ** root);
 
-/*    Function: printnext
- *    Arguments: (struct stack ** root) Pointer to the root node, the top of stack ;
- *    Return: (void)
- *    Description: Called by the print function, to print all nodes other than the topmost */
-void printnext(struct stack ** root);
+/////////////////////////////////// Maze Functions //////////////////////////////////////////////
+/*    Function: recursiveFree
+ *   Arguments: (struct stack * freeMe)  linked list stack of the queues to free
+ *      Return: (void)          no need to return anything just free everything
+ * Description: Get to the bottom of the linked list, then free as coming down call stack */
+void recursiveFree(struct stack * freeMe);
 
-/*    Function: print
- *    Arguments: (struct stack ** root) Pointer to the pointer that points to the top of stack
- *    Return: (void)
- *    Description: Wrapper function, prints the first element and if there is more, then call printnext */
-void print(struct stack ** root);
+/*    Function: callCases
+ *   Arguments: (int numCases)  integer of number of input cases
+ *      Return: (void)          no need to return anything just repeat until numCases == 0
+ * Description: Keeps finding answers for input cases until numCases == 0 */
+void callCases(int numCases);
 
-struct stack * pushBottom(struct stack ** root, struct stack * newBottom );
+/*    Function: recursiveRead
+ *   Arguments: (char ** charSet)  a set of set of chars (aka an array of strings )
+ *              (int ** nBoard)    array of the map filled with ints
+ *      Return: (void)             no need to return anything, we passed by reference
+ * Description:  while numLines isn't zero, it will read a line into board, then iterate to fill nBoard with
+ *               -1 while seeing if board contains 'S', where it will insert a 0 into nBoard, and then recurse */
+void recursiveRead(char ** charSet, int ** nBoard, int numLines,int cols);
 
-void printCC(char ** sets);
-char ** recursiveRead(char ** sets , int ** nBoard, int numSets,int cols);
-struct stack * alloc(int x, int y );
-void spots(struct stack * queue, char ** board,int ** nBoard);
+/*    Function: findExit
+ *   Arguments: (struct stack ** queue)  linked list stack of the queues
+ *              (char ** board)          char dbl array of the map filled
+ *              (int ** nBoard)          int dbl array of the map filled with ints
+ *      Return: (void)          no need to return anything just need to print the answer
+ * Description:  while numLines isn't zero, it will read a line into board, then iterate to fill nBoard with
+ *               -1 while seeing if board contains 'S', where it will insert a 0 into nBoard, and then recurse */
+void findExit(struct stack * queue, char ** board,int ** nBoard);
+
+/////////////////////////////////// Maze Functions END //////////////////////////////////////////////
+
 int main(){
+    // Read first line containing the number of test cases and call callCases with it to find the answers.
+    // then call it a day..
     int firstLine ;
     scanf("%d",&firstLine);
+    callCases(firstLine);// ;P
+}
+
+void callCases(int numCases){
+    if ( numCases == 0 ){
+        return ;
+    }
+    // Read the line containing the rows and columns
     int rows, cols;
     scanf("%d %d",&rows,&cols);
 
+    // Read the input file into board
     char ** board = (char **)malloc(sizeof(char *)*(rows+1));
     int ** nBoard = (int **)malloc(sizeof(int *)*(rows+1));
     recursiveRead(board,nBoard,rows,cols);
-    printCC(board);
-
 
     int i = 0 ;
     int j = 0 ;
+
+    // Use the char array to find the coord of the 'S' character and queue the first spot
     struct stack * start ;
     for ( i = 0 ; i < rows ; i++){
         for ( j = 0 ; j < cols ; j++){
@@ -90,12 +105,29 @@ int main(){
             }
         }
     }
-    spots(start,board,nBoard) ;
+    // Find the shortest distance to get out of maze (if there is one), print -1 if not. Then free all the resources
+    findExit(start,board,nBoard) ;
     free(nBoard);
     free(board);
+    recursiveFree(start);
+
+    // Recursive call
+    callCases(numCases-1);
 }
-
-
+void recursiveFree(struct stack * freeMe){
+    if (freeMe!= NULL){
+        if ( freeMe->next == NULL ){
+            free(freeMe->next);
+        }
+        else{
+            recursiveFree(freeMe->next);
+            free(freeMe);
+        }
+    }
+    else{
+        return ;
+    }
+}
 struct stack * alloc(int x, int y ){
 	struct stack * new = (struct stack *) malloc(sizeof(struct stack));
 	new->next = NULL ;
@@ -103,7 +135,6 @@ struct stack * alloc(int x, int y ){
 	new->coord[1] = y ;
 	return new ;
 }
-
 struct stack * push(struct stack * root, struct stack * newTop){
 	if ( root==NULL){
 		root = newTop ;
@@ -114,21 +145,6 @@ struct stack * push(struct stack * root, struct stack * newTop){
 		return newTop ;
 	}
 }
-struct stack * pushBottom(struct stack ** root, struct stack * newBottom ){
-    if (root != NULL){
-        if ( (*root)->next == NULL ){
-            (*root)->next = newBottom ;
-        }
-        else{
-            pushBottom(&((*root)->next),newBottom);
-        }
-    }
-    else{
-        (*root) = newBottom;
-    }
-    return *root ;
-}
-
 struct stack * pop(struct stack ** root){
     if (root!=NULL ){
         struct stack * popped = (*root);
@@ -140,53 +156,16 @@ struct stack * pop(struct stack ** root){
         return NULL ;
     }
 }
-
-struct stack * popBottom(struct stack ** root){
-    if ((*root)==NULL) return NULL ;
-    else{
-        if ( (*root)->next->next == NULL ){
-            struct stack * bottom = (*root)->next;
-            (*root)->next = (*root)->next->next ;
-            return bottom ;
-        }
-        return popBottom(&((*root)->next));
-    }
-}
-
-struct stack * getBottomPtr(struct stack ** root){
-	if ( (*root)->next->next == NULL ){
-		struct stack * bottom = (*root)->next;
-		return bottom ;
-	}
-    return getBottomPtr(&((*root)->next));
-}
-
-void printnext(struct stack ** root){
-	if ( *root!=NULL ){
-		if ( (*root)->next == NULL ) printf("\e[38;5;26m(%d %d)\e[0m\n",(*root)->coord[0],(*root)->coord[1]);
-		else{
-			printf("(%d %d)",(*root)->coord[0],(*root)->coord[1]);
-			printnext(&((*root)->next));
-		}
-	}
-	else{
-		printf("");
-	}
-}
-
-void print(struct stack ** root){
-	printf("\e[38;5;26m(%d %d)\e[0m",(*root)->coord[0],(*root)->coord[1]);
-	printnext(&((*root)->next));
-}
-
-char ** recursiveRead(char ** sets , int ** nBoard, int numSets,int cols){
+void recursiveRead(char ** sets , int ** nBoard, int numLines, int cols){
     // Base case
-    if (numSets == 0 ) return sets ;
+    if (numLines == 0 ) return ;
 
+    // No particular reason for the calloc.
     *sets = (char *)calloc(cols+1,sizeof(char )) ;
     *nBoard = (int *)calloc(cols+1,sizeof(int )) ;
-    scanf("%s",*sets);
+    scanf("%s",*sets); // Read the whole line
 
+    // find the 'S' and put a 0 in the nBoard there, otherwise -1
     int i = 0 ;
     for (i = 0 ; i < strlen(*sets); i++ ){
         if (*(*(sets)+i)=='S' ){
@@ -197,63 +176,50 @@ char ** recursiveRead(char ** sets , int ** nBoard, int numSets,int cols){
         }
     }
 
-    // Recursive call with the next index of sets and numSets-1
-    recursiveRead(sets+1,nBoard+1,numSets-1,cols);
-
-    // Last one off the stack
-    return sets ;
-}
-void printCC(char ** sets){
-    if ( *sets != NULL ){
-        printf("%s\n",*(sets));
-        printCC(sets+1);
-    }
+    // Recursive call with the next index of and numSets-1
+    recursiveRead(sets+1,nBoard+1,numLines-1,cols);
 }
 
-void spots(struct stack * queue, char ** board,int ** nBoard){
+void findExit(struct stack * queue, char ** board,int ** nBoard){
     //while queue is not Empty
     struct stack * dq ;
     while (queue!= NULL ){
-        // dequeue: pop off the stack current ;
+
+        // dequeue.. Make me have to pop off
         dq = pop(&queue);
 
-        // if this is the goal:
+        // if this is the goal, print the number stored there and get out of here  ;
         if (board[dq->coord[0]][dq->coord[1]] == '~' ){
-            // return ;
+            // print the value stored at the coord and get outta here
             printf("%d \n",nBoard[dq->coord[0]][dq->coord[1]]);
             return;
         }
         // else
         else{
-            // for each neighbor of current, (there can only be four neighbors a most)
-            // if neighbor is unvisited and not blocked then
-            if ( nBoard[dq->coord[0]][dq->coord[1]+1] == -1 && board[dq->coord[0]][dq->coord[1]+1] != 'X' ){
-                // figure out dist diff from current to neighboar and put that val in that index
+            /* In each of the four neighbors of current coordinate, if neighbor is unvisited and not blocked,
+             * then put the number of steps required to move from starting point to there,
+             * which is always going to be the number stored at this coordinate+1, and
+             * enqueue it */
+
+            if ( nBoard[dq->coord[0]][dq->coord[1]+1] == -1 && board[dq->coord[0]][dq->coord[1]+1] != 'X' ){ // Left Adjacent
                 nBoard[dq->coord[0]][dq->coord[1]+1] = nBoard[dq->coord[0]][dq->coord[1]]+1;
-                // enqueue neighbor
                 queue = push(queue,alloc(dq->coord[0],dq->coord[1]+1));
             }
-            if ( nBoard[dq->coord[0]][dq->coord[1]-1] == -1 && board[dq->coord[0]][dq->coord[1]-1] != 'X' ){
-                // figure out dist diff from current to neighboar and put that val in that index
+            if ( nBoard[dq->coord[0]][dq->coord[1]-1] == -1 && board[dq->coord[0]][dq->coord[1]-1] != 'X' ){ // Right Adjacent
                 nBoard[dq->coord[0]][dq->coord[1]-1] = nBoard[dq->coord[0]][dq->coord[1]]+1;
-                // enqueue neighbor
                 queue = push(queue,alloc(dq->coord[0],dq->coord[1]-1));
             }
-            if ( nBoard[dq->coord[0]+1][dq->coord[1]] == -1 && board[dq->coord[0]+1][dq->coord[1]] != 'X' ){
-                // figure out dist diff from current to neighboar and put that val in that index
+            if ( nBoard[dq->coord[0]+1][dq->coord[1]] == -1 && board[dq->coord[0]+1][dq->coord[1]] != 'X' ){ // Top Adjacent
                 nBoard[dq->coord[0]+1][dq->coord[1]] = nBoard[dq->coord[0]][dq->coord[1]]+1;
-                // enqueue neighbor
                 queue = push(queue,alloc(dq->coord[0]+1,dq->coord[1]));
             }
-            if ( nBoard[dq->coord[0]-1][dq->coord[1]] == -1 && board[dq->coord[0]-1][dq->coord[1]] != 'X' ){
-            // mark current as examined
-                // figure out dist diff from current to neighboar and put that val in that index
+            if ( nBoard[dq->coord[0]-1][dq->coord[1]] == -1 && board[dq->coord[0]-1][dq->coord[1]] != 'X' ){ // Bottom Adjacent
                 nBoard[dq->coord[0]-1][dq->coord[1]] = nBoard[dq->coord[0]][dq->coord[1]]+1;
-                // enqueue neighbor
                 queue = push(queue,alloc(dq->coord[0]-1,dq->coord[1]));
             }
         }
     }
-    printf("There is no way out of this maze!\n");
+    // If control reached here, there was no way to get outta here
+    printf("-1\n");
 }
 
