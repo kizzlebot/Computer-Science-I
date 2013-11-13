050dd13c0acc91b620eb6c1abb604e36be368900
James Choi <jchoi2012@knights.ucf.edu>
2013-11-13 05:04:08 -0500

    Update accurateRunTime.c

--- Practice/big_oh/accurateRunTime.c
+++ Practice/big_oh/accurateRunTime.c
@@ -2,7 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 /*
- * > I run this
+- [x] @mentions, #refs, [links](), **formatting**, and <del>tags</del> supported
+- [x] list syntax required (any unordered or ordered list supported)
+- [x] this is a complete item
+- [ ] this is an incomplete item
  */
 int main(){
     int userIn ;
