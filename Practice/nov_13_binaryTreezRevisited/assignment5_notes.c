4c1f8c150fd4c0c88d5a181aaf330bee2f457eee
kizzlebot <jchoi2012@knights.ucf.edu>
2013-11-13 13:25:03 -0500

    after lecture

---
+++ Practice/nov_13_binaryTreezRevisited/assignment5_notes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+ * Cops and robbers
+ *  - Many robbers, running in various directions
+ *  - One cop ( you )
+ *  * Try every ordering of robbers to catch. (permutation)
+ *  * Where will the robber be in t seconds?
+ *      - x = x1 + s1*t*cos(theta)
+ *      - y = y1 + s1*t*sin(theta)
+ *  time = Dist/C_s
+ *
+ */
+
+int main(){
+}
