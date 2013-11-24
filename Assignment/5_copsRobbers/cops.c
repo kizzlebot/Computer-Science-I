d36aea8d0cc18b5e6d799552d9cbb318bd51ecdf
kizzlebot <jchoi2012@knights.ucf.edu>
2013-11-23 23:00:08 -0500

    finito, need to debug assignment5

--- Assignment/5_copsRobbers/cops.c
+++ Assignment/5_copsRobbers/cops.c
@@ -2,149 +2,138 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
+#include "cops.h"
 #define PI 3.14159265358979323846
 
-struct robbers{
-  int x , y , spd , dir ;
-  struct robbers * next ;
-  int num ;
-};
 
-struct robbers * init(int posx,int posy,int speed, int dir,int num);
-float getNewPos(float initPos,int xOrY, float velocity,float time, float direction);
-struct robbers * readIn(int numLines);
-struct robbers * insertBack(struct robbers * root,struct robbers * toInsert);
-float getMinTime(float cop[], float r0[]);
-void print(struct robbers * rob);
-int ** fillPerm(int numElement);
-int * ithPermutation(const int n, int i);
-void printPerm(int ** perm,int numSets,int numElements);
 
-int * ithPermutation(const int n, int i);
-int main(){
-  int copSpeed;
-  int numRobbers = 0 ;
-  scanf("%d %d",&numRobbers,&copSpeed);
-
-  struct robbers * rob = readIn(numRobbers);
-  print(rob);
-
-	int f = factorial(numRobbers);
-	int ** perms = fillPerm(numRobbers);
-	printPerm(perms,factorial(numRobbers),numRobbers);
-}
-void printPerm(int ** perm,int numSets,int numElements){	
-	int i = 0 ; 
-	for ( i = 0 ; i<numSets ; i++){
-		int j = 0 ;
-		for ( j = 0 ; j < numElements ; j++){
-			printf("%d ",perm[i][j]);
-		}
-		printf("\n");
-	}
-}
-int ** fillPerm(int numElement){
-	int ** perms = (int **)malloc(sizeof(int *)*factorial(numElement));
-	int i = 1;
-	int numPerms = factorial(numElement); 
-	for ( i = 0 ; i < numPerms; i++ ){
-		*(perms+i) = ithPermutation(numElement,i+1);
-	}
-	return perms ; 
-}
 int factorial(int n){
-	if ( n <=1 ) return 1 ; 
-	else return n*factorial(n-1);
+  if ( n == 0 || n==1){
+    return 1 ;
+  }
+  else{
+    return n*factorial(n-1);
+  }
 }
-int * ithPermutation(const int n, int i){
-   int j, k = 0;
-   int *fact = (int *)calloc(n, sizeof(int));
-   int *perm = (int *)calloc(n, sizeof(int));
 
-   // compute factorial numbers
-   fact[k] = 1;
-   while (++k < n)
-      fact[k] = fact[k - 1] * k;
+int * ithPermutation(int n, int i){
+  int j, k = 0;
+  int *fact = (int *)calloc(n, sizeof(int));
+  int *perm = (int *)calloc(n, sizeof(int));
 
-   // compute factorial code
-   for (k = 0; k < n; ++k){
+  // compute factorial numbers
+  fact[k] = 1;
+  while (++k < n)
+    fact[k] = fact[k - 1] * k;
+
+  // compute factorial code
+  for (k = 0; k < n; ++k){
       perm[k] = i / fact[n - 1 - k];
       i = i % fact[n - 1 - k];
-   }
+  }
 
-   // readjust values to obtain the permutation
-   // start from the end and check if preceding values are lower
-   for (k = n - 1; k > 0; --k)
-      for (j = k - 1; j >= 0; --j)
-         if (perm[j] <= perm[k])
-            perm[k]++;
+  // readjust values to obtain the permutation
+  // start from the end and check if preceding values are lower
+  for (k = n - 1; k > 0; --k)
+    for (j = k - 1; j >= 0; --j)
+      if (perm[j] <= perm[k])
+        perm[k]++;
 
-
-   free(fact);
-   return perm ; 
+  free(fact);
+  return perm ;
 }
-struct robbers * readIn(int numLines){
-  int info[4] ;
+
+void readRobbers(Robber ** rob , int numRobbers){
+  *rob = (Robber *) calloc((numRobbers+1),sizeof(Robber));
+
   int i = 0 ;
-  struct robbers * rob;
-  int num = 0 ;
+  int x, y , spd , dir ;
+  x = 0 ;
+  y = 0 ;
+  spd = 0 ;
+  dir = 0 ;
+  for ( i = 0 ; i < numRobbers ; i++ ){
+    scanf("%d %d %d %d",&x,&y,&dir,&spd);
+    *(*rob+i) = initRobber(x,y,spd,dir);
+  }
+}
+void readAndExecute(){
+  int numRobbers ;
+  int spd = 0 ;
+  while (!feof(stdin)){
+    scanf("%d %d",&numRobbers,&spd);
+    if (numRobbers == 0 && spd == 0) break;
+    Cop copper ;
+    copper.x = 0 ;
+    copper.y = 0 ;
+    copper.time = 0.0  ;
+    copper.speed = spd ;
 
-  int x, y , spd, dir;
-  for ( i = 0 ; i < numLines ; i++ ){
-    scanf("%d %d %d %d",&x,&y,&spd,&dir);
-    rob = insertBack(rob,init(x,y,spd,dir,i));
-  }
-  return rob ;
-}
-void print(struct robbers * rob){
-  if (rob == NULL ) return ;
-  else{
-    printf("%d %d %d %d\n",rob->x,rob->y,rob->spd,rob->dir);
-    print(rob->next);
-  }
-}
-float getMinTime(float cop[], float r0[]){
-  float minTime = 0.0 ;
+    Robber * rob ;
+    readRobbers(&rob,numRobbers);
+    double time =0;
+    double minTime = 0 ;
+    int j = 0 ;
 
-  float rx = 30 ;
-  float ry = 30 ;
-  float distChase = 1;
-  while ( cop[2]*minTime < distChase){
-    minTime+=0.0000001 ;
-    distChase = pow((pow((getNewPos(r0[0],0,r0[2],minTime,r0[3])-cop[0]),2)+pow((getNewPos(r0[1],1,r0[2],minTime,r0[3])-cop[1]),2)),0.5);
+    int fact = factorial(numRobbers);
+
+    int i = 0 ;
+    int * perm ;
+    // try every permutation
+    for ( j = 0 ; j < fact ; j++ ){
+      perm = ithPermutation(numRobbers,j);
+      copper.x = 0 ;
+      copper.y = 0 ;
+      copper.time = 0.0 ;
+
+      for ( i = 0 ; i < numRobbers ; i++){
+        time=getMinTime(copper,rob[perm[i]]) ;
+        copper.time+=time ;
+        copper.x = rob[perm[i]].x+rob[perm[i]].speed*(copper.time)*cos((rob[perm[i]].direction*2*PI)/360);
+        copper.y = rob[perm[i]].y+rob[perm[i]].speed*(copper.time)*sin((rob[perm[i]].direction*2*PI)/360);
+        //printf("(%d) Robber: %d %d %d %d Time: %f\n",i,rob[i].x,rob[i].y,rob[i].speed,rob[i].direction,time);
+      }
+      if ( j == 0 ) minTime = copper.time ;
+      else{
+        if ( copper.time < minTime) minTime = copper.time ;
+      }
+      free(perm);
+    }
+    printf("minTime:%f\n",minTime);
+    free(rob);
   }
-  while ( cop[2]*minTime > distChase){
-    minTime-=0.0000001 ;
-    distChase = pow((pow((getNewPos(r0[0],0,r0[2],minTime,r0[3])-cop[0]),2)+pow((getNewPos(r0[1],1,r0[2],minTime,r0[3])-cop[1]),2)),0.5);
-  }
-  return minTime ;
 }
-float getNewPos(float initPos,int xOrY, float velocity,float time, float direction){
-  float rtn[2] ;
-  if ( xOrY == 0 )
-    return initPos+velocity*time*cos((direction*2*PI)/360);
-  else {
-    return initPos+velocity*time*sin((direction*2*PI)/360);
+double getMinTime(Cop copper,Robber rob){
+  double distChase =10;
+  // use copper.time to figure robber pos.
+  double time = 0.0;
+  // while loop until copper.speed*t > distance
+
+  float inc = 10 ;
+  while ( 1 ){
+    while (copper.speed*(time) < distChase){
+      time+=inc;
+      distChase = pow((pow((rob.x+rob.speed*(copper.time+time)*cos((rob.direction*2*PI)/360)-copper.x),2)+pow((rob.y+rob.speed*(copper.time+time)*sin((rob.direction*2*PI)/360)-copper.y),2)),0.5);
+    }
+    inc/=2;
+    if (copper.speed*time-distChase < 0.0000001) break;
+    while (copper.speed*(time) > distChase ){
+      time-=inc;
+      distChase = pow((pow((rob.x+rob.speed*(copper.time+time)*cos((rob.direction*2*PI)/360)-copper.x),2)+pow((rob.y+rob.speed*(copper.time+time)*sin((rob.direction*2*PI)/360)-copper.y),2)),0.5);
+    }
+    inc/=2;
+    if (distChase-copper.speed*time< 0.0000001) break;
   }
-}
-struct robbers * init(int posx,int posy,int speed, int dir,int num){
-  struct robbers * rob = (struct robbers *)malloc(sizeof(struct robbers));
-  rob->next = NULL ;
-  rob->x=posx;
-  rob->y=posy;
-  rob->spd=speed;
-  rob->dir=dir;
-  rob->num = num ;
-  return rob;
+  return time ;
 }
 
-struct robbers * insertBack(struct robbers * root,struct robbers * toInsert){
-  if ( root == NULL ){
-    return toInsert ;
-  }
-  else{
-    root->next = insertBack(root->next,toInsert);
-    return root ;
-  }
+Robber initRobber(int x, int y, int speed, int direction){
+  Robber rtn  ;
+  rtn.x = (float)x ;
+  rtn.y = (float)y ;
+  rtn.speed = (float)speed ;
+  rtn.direction = (float)direction ;
+  return rtn  ;
 }
+
 
