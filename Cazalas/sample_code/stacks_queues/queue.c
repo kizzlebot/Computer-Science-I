9233cdd32a9d2a77e1be6d9231839d65634c5369
kizzlebot <jchoi2012@knights.ucf.edu>
2014-01-21 03:32:24 -0500

    arup code is ugly as hell

--- Cazalas/sample_code/stacks_queues/queue.c
+++ Cazalas/sample_code/stacks_queues/queue.c
@@ -2,6 +2,7 @@
 // 6/26/07
 // Example of how to implement a queue with an array.
 #include <stdio.h>
+#include <stdlib.h>
 
 #define EMPTY -1
 #define INIT_SIZE 10
@@ -21,44 +22,44 @@ int empty(struct queue* qPtr);
 int front(struct queue* qPtr);
 
 int main() {
-     
+
      int i;
-     
+
     // Allocate space for our queue and initialize it.
     struct queue* MyQueuePtr = (struct queue*)malloc(sizeof(struct queue));
     init(MyQueuePtr);
-    
+
     // Enqueue some items.
     enqueue(MyQueuePtr, 3);
     enqueue(MyQueuePtr, 7);
     enqueue(MyQueuePtr, 4);
-    
+
     // Try one dequeue.
     printf("Dequeue %d\n", dequeue(MyQueuePtr));
-    
+
     // Enqueue one more item, then try several dequeues and one front.
     enqueue(MyQueuePtr, 2);
     printf("Dequeue %d\n", dequeue(MyQueuePtr));
     printf("At Front of Queue Now: %d\n", front(MyQueuePtr));
     printf("Dequeue %d\n", dequeue(MyQueuePtr));
     printf("Dequeue %d\n", dequeue(MyQueuePtr));
-    
+
     // See if we can detect an empty queue.
     printf("Is empty: %d\n", empty(MyQueuePtr));
-    
+
     // Try enqueuing and dequeuing again to make sure that our previous
     // operations didn't "corrupt" the queue.
     enqueue(MyQueuePtr, 5);
     enqueue(MyQueuePtr, 9);
-    
+
     // Try lots of enqueues to test the dynamic capability of the queue.
     for (i=30; i<60; i++)
         enqueue(MyQueuePtr, i);
-        
+
     // Dequeue everything.
     while (!empty(MyQueuePtr))
         printf("Dequeue %d\n", dequeue(MyQueuePtr));
-    
+
     system("PAUSE");
     return 0;
 }
@@ -67,7 +68,7 @@ int main() {
 // Post-condition: The struct that qPtr points to will be set up to represent an
 //                 empty queue.
 void init(struct queue* qPtr) {
-     
+
      // The front index is 0, as is the number of elements.
      qPtr->elements = (int*)malloc(sizeof(int)*INIT_SIZE);
      qPtr->front = 0;
@@ -86,66 +87,66 @@ void init(struct queue* qPtr) {
 int enqueue(struct queue* qPtr, int val) {
 
     int i;
-    
+
     // Regular case where our queue isn't full.
     if (qPtr->numElements != qPtr->queueSize) {
-             
+
         // Enqueue the current element. Note the use of mod for the wraparound
         // case. Edit the number of elements.
         qPtr->elements[(qPtr->front+qPtr->numElements)%qPtr->queueSize] = val;
         (qPtr->numElements)++;
-         
+
         // Signifies a successful operation.
         return 1;
     }
-    
+
     // Case where the queue is full, we must find more space before we enqueue.
     else {
-         
+
          // Allocate more space!
          realloc(qPtr->elements, (qPtr->queueSize)*sizeof(int)*2);
-         
+
          // Copy all of the items that are stored "before" front and copy them
          // into their new correct locations.
          for (i=0; i<=qPtr->front-1; i++)
-             qPtr->elements[i+qPtr->queueSize] = qPtr->elements[i];     
-         
+             qPtr->elements[i+qPtr->queueSize] = qPtr->elements[i];
+
          // Enqueue the new item, now that there is space. We are guaranteed that
          // no wraparound is necessary here.
-         qPtr->elements[i+qPtr->queueSize] = val;    
-         
+         qPtr->elements[i+qPtr->queueSize] = val;
+
          // More bookkeeping: The size of the queue as doubled and the number of
          // elements has increased by one.
          (qPtr->queueSize) *= 2;
          (qPtr->numElements)++;
-         
-         return 1;    
+
+         return 1;
     }
-        
+
 }
 
 // Pre-condition: qPtr points to a valid struct queue.
 // Post-condition: If the queue pointed to by qPtr is non-empty, then the value
-//                 at the front of the queue is deleted from the queue and 
+//                 at the front of the queue is deleted from the queue and
 //                 returned. Otherwise, -1 is returned to signify that the queue
 //                 was already empty when the dequeue was attempted.
 int dequeue(struct queue* qPtr) {
-    
+
     int retval;
-    
+
     // Empty case.
     if (qPtr->numElements == 0)
         return EMPTY;
-        
+
     // Store the value that should be returned.
     retval = qPtr->elements[qPtr->front];
-    
+
     // Adjust the index to the front of the queue accordingly.
     qPtr->front = (qPtr->front + 1)% qPtr->queueSize;
-    
+
     // We have one fewer element in the queue.
     (qPtr->numElements)--;
-    
+
     // Return the dequeued element.
     return retval;
 }
@@ -157,7 +158,7 @@ int empty(struct queue* qPtr) {
 }
 
 // Pre-condition: qPtr points to a valid struct queue.
-// Post-condition: if the queue pointed to by qPtr is non-empty, the value 
+// Post-condition: if the queue pointed to by qPtr is non-empty, the value
 //                 stored at the front of the queue is returned. Otherwise,
 //                 -1 is returned to signify that this queue is empty.
 int front(struct queue* qPtr) {
--- Sample_Code/Recursion/permute2.c
+++ Sample_Code/Recursion/permute2.c
@@ -10,27 +10,27 @@ void RecursivePermute(char str[], int k);
 void ExchangeCharacters(char str[], int i, int j);
 
 int main() {
-    
+
     char word[20];
-    
+
     // Let the user enter a word to permute.
     printf("Please enter a word you would like to permute.\n");
     scanf("%s", word);
-    
+
     // Print out the permutations.
     printf("\nHere are the permutations:\n\n");
     ListPermutations(word);
-    
+
     system("PAUSE");
     return 0;
-    
+
 }
 
 // Pre-condition: str is a valid C String.
 // Post-condition: All permutations of str (assuming all distinct
 //                 characters) will be printed.
 void ListPermutations(char str[]) {
-     
+
      // Call the appropriate recursive function with the correct
      // parameters.
      RecursivePermute(str, 0);
@@ -43,52 +43,52 @@ void ListPermutations(char str[]) {
 //                 printed. Namely, if n is the length of str, then
 //                 (n-k)! permutations are printed.
 void RecursivePermute(char str[], int k) {
-     
-     int j, i;
 
-     // Printing out recursion information for non-trivial calls.
-     if (k < strlen(str)) {
+  int j, i;
 
-       // This prints out spaces so we can visualize the depth in the
-       // recursive stack with the printout.
-       for (i=0; i<k; i++)
-         printf("  ");
-     
-       // Prints out the fixed portion of the string for this recursive call.
-       printf("Calling with ");
-       for (i=0; i<k; i++)
-         printf("%c", str[i]);
-     
-       // Now print out the letters we are permuting.
-       printf(" fixed. Permute: ");
-       for (i=k; i<strlen(str); i++)
-         printf("%c", str[i]);
-       printf("\n");
-     }
+  // Printing out recursion information for non-trivial calls.
+  if (k < strlen(str)) {
 
-     // Base-case: Since all letters are fixed, we can ONLY print
-     // what's stored in str.
-     if (k == strlen(str))
-         printf("%s\n", str);
-         
-     else {
-          
-         // Loop through each possible starting letter for index k,
-         // the first index for which we have a choice.
-         for (j=k; j<strlen(str); j++) {
-         
-             // Place the character stored in index j in location k.
-             ExchangeCharacters(str, k, j);
-             
-             // Print out all of the permutations with that character
-             // just chosen above fixed. 
-             RecursivePermute(str, k+1);
-             
-             // Put the original character that used to be there back
-             // in its place.
-             ExchangeCharacters(str, j, k);
-         }
-     }
+    // This prints out spaces so we can visualize the depth in the
+    // recursive stack with the printout.
+    for (i=0; i<k; i++)
+      printf("  ");
+
+    // Prints out the fixed portion of the string for this recursive call.
+    printf("Calling with ");
+    for (i=0; i<k; i++)
+      printf("%c", str[i]);
+
+    // Now print out the letters we are permuting.
+    printf(" fixed. Permute: ");
+    for (i=k; i<strlen(str); i++)
+      printf("%c", str[i]);
+    printf("\n");
+  }
+
+  // Base-case: Since all letters are fixed, we can ONLY print
+  // what's stored in str.
+  if (k == strlen(str))
+    printf("%s\n", str);
+
+  else {
+
+    // Loop through each possible starting letter for index k,
+    // the first index for which we have a choice.
+    for (j=k; j<strlen(str); j++) {
+
+      // Place the character stored in index j in location k.
+      ExchangeCharacters(str, k, j);
+
+      // Print out all of the permutations with that character
+      // just chosen above fixed.
+      RecursivePermute(str, k+1);
+
+      // Put the original character that used to be there back
+      // in its place.
+      ExchangeCharacters(str, j, k);
+    }
+  }
 }
 
 // Pre-condition: str is a valid C String and i and j are valid indexes
@@ -96,8 +96,8 @@ void RecursivePermute(char str[], int k) {
 // Post-condition: The characters at index i and j will be swapped in
 //                 str.
 void ExchangeCharacters(char str[], int i, int j) {
-     
-    char temp = str[i];
-    str[i] = str[j];
-    str[j] = temp;
+
+  char temp = str[i];
+  str[i] = str[j];
+  str[j] = temp;
 }
