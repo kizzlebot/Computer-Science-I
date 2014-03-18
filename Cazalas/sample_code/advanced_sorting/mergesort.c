cb3cece1a856ff949a997ab9d5d9acb4d7cd77e9
kizzlebot <jchoi2012@knights.ucf.edu>
2014-03-18 10:46:12 -0400

    update mergesort sample

--- Cazalas/sample_code/advanced_sorting/mergesort.c
+++ Cazalas/sample_code/advanced_sorting/mergesort.c
@@ -17,7 +17,7 @@ void Merge(int values[], int start, int middle, int end);
 int Is_Sorted(int values[], int length);
 
 int main() {
-  
+
 
     int *val;
     val = (int*)(malloc(sizeof(int)*SIZE));
@@ -29,15 +29,15 @@ int main() {
 
     // And now, the sorted ones.
     MergeSort(val, 0, SIZE-1);
-    //Print_Array(val, SIZE);
+    Print_Array(val, SIZE);
 
     // Check if it's sorted.
     if (Is_Sorted(val, SIZE))
         printf("Sorted correctly.\n");
-        
+
     free(val);
     system("PAUSE");
-  
+
     return 0;
 }
 
@@ -52,9 +52,9 @@ void Print_Array(int values[], int length) {
     printf("\n");
 }
 
-// Pre-condition: length is the length of the array values and max<32767.
-// Post-condition: the array values will be initialized with random values
-//                 in between 1 and max.
+/* Pre-condition: length is the length of the array values and max<32767.
+ * Post-condition: the array values will be initialized with random values
+ *                 in between 1 and max. */
 void Fill_Array(int values[], int length, int max) {
 
     int i;
@@ -65,14 +65,14 @@ void Fill_Array(int values[], int length, int max) {
 // Pre-condition: values is of length length.
 // Post-condition: Returns 1, iff values is in non-decreasing order.
 int Is_Sorted(int values[], int length) {
-    
+
     int i;
-    
+
     // Return false if any adjacent pair is out of order.
     for (i=0; i<length-1; i++)
         if (values[i] > values[i+1])
             return 0;
-            
+
     return 1;
 }
 
@@ -82,35 +82,35 @@ int Is_Sorted(int values[], int length) {
 void MergeSort(int values[], int start, int end) {
 
     int mid;
-  
+
     // Check if our sorting range is more than one element.
     if (start < end) {
 
         mid = (start+end)/2;
-    
+
         // Sort the first half of the values.
         MergeSort(values, start, mid);
-    
+
         // Sort the last half of the values.
         MergeSort(values, mid+1, end);
-    
+
         // Put it all together.
         Merge(values, start, mid+1, end);
     }
 }
 
-// Pre-condition: start, middle and end are valid indexes to the array values,
-//                with middle >= start and middle <= end. Also, the values from
-//                index start to middle are sorted AND the values from middle+1
-//                to end are sorted.
-// Post-condition: The values in the array starting from index start to
-//                 index end will be in non-decreasing sorted order.
+/* Pre-condition: start, middle and end are valid indexes to the array values,
+ *                with middle >= start and middle <= end. Also, the values from
+ *                index start to middle are sorted AND the values from middle+1
+ *                to end are sorted.
+ * Post-condition: The values in the array starting from index start to
+                   index end will be in non-decreasing sorted order.*/
 void Merge(int values[], int start, int middle, int end) {
 
     //printf("merge %d, %d, %d\n", start, middle, end);
-    
+
     int *temp, i, length, count1, count2, mc;
-  
+
     // Allocate the proper amount of space for our auxiliary array.
     length = end - start + 1;
     temp = (int*)calloc(length, sizeof(int));
@@ -118,23 +118,23 @@ void Merge(int values[], int start, int middle, int end) {
     // These will be our indexes into our two sorted lists.
     count1 = start;
     count2 = middle;
-  
+
     // Keeps track of our index into our auxiliary array.
     mc = 0;
 
-    // Here we copy values into our auxiliary array, so long as there are 
-    // numbers from both lists to copy.
+    /* Here we copy values into our auxiliary array, so long as there are
+     * numbers from both lists to copy.*/
     while ((count1 < middle) || (count2 <= end)) {
 
-        // Next value to copy comes from list one - make sure list
-        // one isn't exhausted yet. Also make sure we don't access index
-        // count2 if we aren't supposed to.
+        /* Next value to copy comes from list one - make sure list
+         * one isn't exhausted yet. Also make sure we don't access index
+         * count2 if we aren't supposed to.*/
         if (count2 > end || (count1 < middle && values[count1] < values[count2])) {
             temp[mc] = values[count1];
             count1++;
             mc++;
         }
-    
+
         // We copy the next value from list two.
         else {
             temp[mc] = values[count2];
